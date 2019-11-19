f434932643e414323972bf6c9ac6345c38455df5
Jeff Cotton (via Travis CI) <jeff.cotton@gmail.com>
2019-11-19 23:49:01 +0000

    Deploy blunt/mbg-flipp to github.com/blunt/mbg-flipp.git:master

--- src/js/functions.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelectorAll('.toggleFlip__button');
-
-for (const btn of button) {
-    btn.addEventListener('click', function (event) {
-        event.preventDefault();
-        this.nextElementSibling.classList.toggle('flipped');
-        this.classList.toggle('active');
-    })
-}
